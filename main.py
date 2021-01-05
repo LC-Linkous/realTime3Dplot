cc08bb3b8592d7ad978d7339af30c7bd541a307e
Lauren <l.c.linkous@gmail.com>
2021-01-04 20:00:51 -0500

    formatting update

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from viewer import Viewer
 import matplotlib
 import time
 
-if __name__ == '__main__':
+def main():
     matplotlib.interactive(True)
     v = Viewer()
     while True:
         v.drawNow()
         time.sleep(.1)
+
+
+if __name__ == '__main__':
+    main()
 
